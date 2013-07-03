8dfb06b728b0bb30d7715693c5923d793c4e17c6
ssaaurabh <saurabh.singla@intuzion.com>
2013-07-03 14:58:19 +0530

    unread conversations displayed on index page

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -48,9 +48,37 @@ class ConversationsController < ApplicationController
 		#@conversations = current_user.mailbox.conversations
 
 		@inbox = current_user.mailbox.inbox
+		@countInboxConvUnread = 0
+		@inbox.each do |inbox|
+			inbox.receipts.each do |receipt|
+				if !receipt.is_read? && receipt.receiver_id == current_user.id
+					@countInboxConvUnread = @countInboxConvUnread + 1
+					break
+				end
+			end			
+		end
 		# debugger
 		@sentbox = current_user.mailbox.sentbox
+		@countSentboxConvUnread = 0
+		@sentbox.each do |sentbox|
+			sentbox.receipts.each do |receipt|
+				if !receipt.is_read? && receipt.receiver_id == current_user.id
+					@countSentboxConvUnread = @countSentboxConvUnread + 1
+					break
+				end
+			end			
+		end
+
 		@trash = current_user.mailbox.trash
+		@countTrashConvUnread = 0
+		@trash.each do |trash|
+			trash.receipts.each do |receipt|
+				if !receipt.is_read? && receipt.receiver_id == current_user.id
+					@countTrashConvUnread = @countTrashConvUnread + 1
+					break
+				end
+			end			
+		end
 	end
 
 	# to trash a particular conversation
